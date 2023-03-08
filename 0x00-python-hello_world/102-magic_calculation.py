392f72e44d750bc7cf649905a59990418fb05848
Milemotigo <mtigo3617@gmail.com>
2023-03-07 16:39:53 -0800

    mm

--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def magic_calculation(a, b):
-    #return(98 + (a ** b))
-    resut = 98
-    result += pow(a, b)
-    return result
+    return(98 + (a ** b))
+    #resut = 98
+    #result += pow(a, b)
+    #return result
