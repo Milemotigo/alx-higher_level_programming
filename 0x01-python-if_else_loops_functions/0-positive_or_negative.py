ac37d13e0fae4bb52438bb75bdb2fb2959a6b2d8
Milemotigo <mtigo3617@gmail.com>
2023-03-09 01:57:09 -0800

    absolute_values

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 import random
-# Generate a random number between -10 and 10 inclusive
 number = random.randint(-10, 10)
-# Check if the number is positive
 if number > 0:
     print(f"{number} is positive")
 elif number == 0:
     print(f"{number} is zero")
-# check if the above statement is not satisfied which means the number is negative
 else:
     print(f"{number} is negative")
