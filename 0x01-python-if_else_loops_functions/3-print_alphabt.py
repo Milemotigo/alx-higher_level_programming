a4beb3d2ea682f4d201c0028c3c397d3ab1804d7
Milemotigo <mtigo3617@gmail.com>
2023-03-09 03:16:42 -0800

    if_or_except

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
-#!/usr/bin/bash
-
+#!/usr/bin/python3
 for i in range(97, 123):
-    if chr(i) == 'e' or chr(i) == 'q':
+    if chr(i) == 'q' or chr(i) == 'e':
         continue
     print(chr(i).format(i), end =" ")
