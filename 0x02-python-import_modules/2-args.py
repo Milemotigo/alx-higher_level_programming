2a2a5c207c9d140c69813b32d780f425d4334f1d
Milemotigo <mtigo3617@gmail.com>
2023-03-11 03:26:20 -0800

    ammend

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-if __name__=='__main__':
-    import sys
-    args = sys.argv
-    num_args = len(args) - 1
-    if num_args == 0:
-        print("0 arguments")
-    else:
-        print(f"{num_args} argument{'s' if num_args>1 else''}")
-        for i in range(num_args):
-            print("{}: {}".format(i + 1, args[i + 1]))
+
+import sys
+
+count = len(sys.argv) - 1
+if count == 0:
+    print("0 arguments.")
+elif count == 1:
+    print("1 argument:")
+else:
+    print("{} arguments:".format(count))
+for i in range(count):
+     print("{}: {}".format(i + 1, sys.argv[i + 1]))
