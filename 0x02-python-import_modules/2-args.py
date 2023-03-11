4fa60b3b3c70b0b9f5c09f183f764cd542f2dd8d
milemotigo <mtigo3617@gmail.com>
2023-03-11 14:49:06 +0000

    python

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
-
+if __name__ == '__main__':
 import sys
-
-count = len(sys.argv) - 1
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
+args = argv[1:]
+num_args = args
+if num_args == 0:
+print("0 argument")
 else:
-    print("{} arguments:".format(count))
-for i in range(count):
-     print("{}: {}".format(i + 1, sys.argv[i + 1]))
+print(f"{num_args} argument{'s' if num_args > 1) else '':}")
+for i, arg in enumerate(args):
+print(f"{i + 1}: {args}")
