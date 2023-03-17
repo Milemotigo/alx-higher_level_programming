c83f3387f0cf6eaf9195d20002f91b1e577e2010
Milemotigo <global-config@mtgo3617@gmail.com>
2023-03-17 13:12:11 -0700

    complex_key-del

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
+def complex_delete(a_dict, value):
     key_to_delete = []
-    for key in a_dictionary:
-        if a_dictionary[key] == value:
+    for key in a_dict:
+        if a_dict[key] == value:
             key_to_delete.append(key)
-            for key in key_to_delete:
-                del a_dictionary[key]
-                return a_dictionary
+        for key in key_to_delete:
+            del a_dict[key]
+        return a_dict
