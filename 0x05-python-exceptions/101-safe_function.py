83340be7030f250c4a1cf8cd31d861db7adf1559
Milemotigo <global-config@mtgo3617@gmail.com>
2023-03-28 05:17:16 -0700

    try_except

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
+
+
 def safe_function(fct, *args):
     try:
-        #attempt to call a function with a given argument
         result = fct(*args)
         return result
     except Exception as e:
-        print("Exception: {}".format(e), file = sys.stderr)
+        print("Exception: {}".format(e), file=stderr)
         return None
