51787a4116e54cfc755de606b0d3c8bc2960046b
Milemotigo <global-config@mtgo3617@gmail.com>
2023-03-29 11:01:17 -0700

    clases_square

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-"""define a class named Square"""
+""" defines a square """
+
 
 class Square:
-    """define a specail model named init that is called when a new instance of the square is created"""
-    def __init__(self, size = 0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+    """ square with private instance attribute size """
+
+    def __init__(self, size=0):
+        """
+        Args:
+            size: size of square
+        """
+
+        if type(size) is int:
+            if size < 0:
+                raise ValueError('size must be >= 0')
+            else:
+                self.__size = size
         else:
-             """create an instance variable named __size and assign it the value size. the self parameter is used to refer to refer to the instance been created"""
-        self.__size = size
+            raise TypeError('size must be an integer')
