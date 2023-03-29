0cfcdee08906c8f5a5cc56763bcf73c5c1869730
Milemotigo <global-config@mtgo3617@gmail.com>
2023-03-29 11:45:19 -0700

    clases_square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 """ defines a square """
 
+
 class Square:
     """ square with private instance attribute size """
     def __init__(self, size=0):
         """check to make sure that the value is an integer"""
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             ValueError("size must be >= 0")
         else:
             self.__size = size
+
     def area(self):
+
         return self.__size ** 2
