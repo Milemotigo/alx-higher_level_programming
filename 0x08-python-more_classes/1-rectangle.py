bb52e4f56e43114a0309c873e6cb987e9487c107
milemotigo <mtigo3617@gmail.com>
2023-04-05 10:27:36 -0700

    indentation

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -5,32 +5,33 @@
 
 class Rectangle:
     def __init__(self, width=0, height=0):
-        self.width = width
         self.height = height
+        self.width = width
 
     @property
     def width(self):
         """getter for private instance width"""
-        return self.width
+        return self.__width
 
     @width.setter
     def width(self, value):
         """setter for private instance width"""
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-        @property
-        def height(self):
-            return self._height
-        """getter for private instance width"""
-        @height.setter
-        def height(self, value):
-            """setter for private instance width"""
-            if not isinstance(value, int):
-                raise TypeError("height must be an integer")
-            if value < 0:
-                raise ValueError("height must be >= 0")
-            self.__height = value
+    @property
+    def height(self):
+        """getter for private instance height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """setter for private instance height"""
+        if  type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
