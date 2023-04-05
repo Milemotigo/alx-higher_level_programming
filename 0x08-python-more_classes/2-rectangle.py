bfba50074e61edd2c83027d248e8ef42a579710d
milemotigo <mtigo3617@gmail.com>
2023-04-05 15:33:14 -0700

    null

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -8,6 +8,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -15,9 +16,11 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -25,9 +28,12 @@ class Rectangle:
         elif value < 0:
             raise ValueError("width must be >= 0")
         self.__height = value
+
     def area(self):
         return self.width * self.height
+
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return  0
+
         return 2 * (self.width + self.height)
