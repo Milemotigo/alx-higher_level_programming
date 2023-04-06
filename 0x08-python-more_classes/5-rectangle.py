3336315e7f2f5096fcefa8da8a12982de03529d8
milemotigo <mtigo3617@gmail.com>
2023-04-06 04:56:53 -0700

    5-

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -55,4 +55,4 @@ class Rectangle:
         return "Rectangle({:d}, {:d})".format(self.width, self.height)
 
     def __del__(self):
-        print("Bye rectangle... (...")
+        print("Bye rectangle... ")
