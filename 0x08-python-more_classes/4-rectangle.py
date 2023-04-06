0554a229d6e2462b72105e1a4de941f02aff7051
milemotigo <mtigo3617@gmail.com>
2023-04-06 04:23:25 -0700

    buytime

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -50,5 +50,6 @@ class Rectangle:
             string += "\n".join("#" * self.__width
                                 for j in range(self.__height))
             return string
+
     def __repr__(self):
         return f"Rectangle({self.width}, {self.height})"
