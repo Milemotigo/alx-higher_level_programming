4a0f898150eff2f2b4fc5590ab95833533e49f07
milemotigo <mtigo3617@gmail.com>
2023-04-05 17:00:54 -0700

    yun

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -30,7 +30,7 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """setter for private instance height"""
-        if not isinstance(valur, int):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
