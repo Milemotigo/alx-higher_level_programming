dffead15df7d5ae85834d578c880a9a6c2bcb58b
milemotigo <mtigo3617@gmail.com>
2023-04-06 07:26:51 -0700

    er

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -51,7 +51,7 @@ class Rectangle:
         string = ""
         if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width
-                                for j in range(self.__height))
+                                for i in range(self.__height))
             return string
 
     def __repr__(self):
