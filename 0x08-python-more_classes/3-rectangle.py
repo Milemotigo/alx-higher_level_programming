6fafdd399f693864e6aabc1c549ac2031d9c0864
milemotigo <mtigo3617@gmail.com>
2023-04-06 04:25:35 -0700

    buytime

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -21,7 +21,8 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__width = value
+        else:
+            self.__width = value
 
     @property
     def height(self):
@@ -33,7 +34,8 @@ class Rectangle:
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
-        self.__height = value
+        else:
+            self.__height = value
 
     def area(self):
         return self.__width * self.__height
