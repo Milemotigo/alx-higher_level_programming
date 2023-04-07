79b2435a8f6eab46a6c703e1abf79c835a6a0a31
milemotigo <mtigo3617@gmail.com>
2023-04-07 07:53:30 -0700

    stalk

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,27 @@
 #!/usr/bin/python3
+"""Module for add_integer method."""
 
 
-def add_integer(a, b):
-    """Return the addition of two numbers."""
-    if not isinstance(a, (int, float)):
+def add_integer(a, b=98):
+    """Adds 2 integers.
+
+    Args:
+        a: the first int.
+        b: second int, default 98.
+
+    Raises:
+        TypeError: if a, b are neither int nor float.
+
+    Returns:
+        sum of the 2 integers.
+    """
+
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
-    if isinstance(a, float):
-        a = int(a)
-    if isinstance(b, float):
-        b = int(b)
-    return a + b
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
