a12d7b82c634fab02cd5bc0f6e6d10010df16d9b
milemotigo <mtigo3617@gmail.com>
2023-04-07 07:44:40 -0700

    stalk

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -3,13 +3,13 @@ add_integer = __import__('0-add_integer').add_integer
 
 print(add_integer(1, 2))
 print(add_integer(100, -2))
-print(add_integer(2))
+print(add_integer(2, 98))
 print(add_integer(100.3, -2))
 try:
-    print(add_integer(4, "School"))
+    print(add_integer("School", 4))
 except Exception as e:
     print(e)
 try:
-    print(add_integer(None))
+    print(add_integer(4, "school"))
 except Exception as e:
-    print(e)
+        print(e)
