4623b86827d305e3b1696c52ad64e4bdd7415a7b
milemotigo <mtigo3617@gmail.com>
2023-04-07 09:28:26 -0700

    add.int

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Module for add_integer method."""
 
 
 def add_integer(a, b=98):
     """Adds 2 integers.
-    Args:
+    Arguments:
         a: the first int.
         b: second int, default 98.
     Raises:
@@ -13,8 +12,8 @@ def add_integer(a, b=98):
         sum of the 2 integers.
     """
 
-    if type(a) not in (int, float):
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
-    if type(b) not in (int, float):
+    if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    return (int(a) + int(b))
