95f4ceed5b50d6422ec2ba68245b5164a30caa76
milemotigo <mtigo3617@gmail.com>
2023-04-16 11:57:44 -0700

    json

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Defines a string json file converter'''
+import json
+
+def to_json_string(my_obj):
+    '''Return json representationnof an object
+    Arg:
+        my_obj: object to convert to json
+    '''
+    return json.dumps(my_obj)
