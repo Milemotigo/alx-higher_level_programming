fb7adc1c331848a54683f49e5cca2c9dd22b9f73
milemotigo <mtigo3617@gmail.com>
2023-04-17 05:37:29 -0700

    args_to_python_script

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,12 +10,13 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == '__main__':
-    file_name = add_item.json
-    j_List = []
+file = "add_item.json"
+json_list = []
 
-    if path.isfile(file_name):
-        j_List = load_from_json_file(file_name)
-        for i in range(1, len(sys.argv)):
-            json_list.append(sys.argv[i])
-        save_to_json_file(j_List, file_name)
+if os.path.exists(file):
+    json_list = load_from_json_file(file)
+
+for i in range(1, len(sys.argv)):
+    json_list.append(sys.argv[i])
+
+save_to_json_file(json_list, file)
