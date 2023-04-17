f38ae698a82614476713f57513d8b2cfd44cc4b7
milemotigo <mtigo3617@gmail.com>
2023-04-17 07:51:12 -0700

    task7

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''creates a pascal triangle'''
+
+
+def pascal_triangle(n):
+    p_list = []
+    
+    for i, n in p_list():
+        return p_list
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-import json
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-from os import path
 
-from save_to_json_file import save_to_json_file
-from load_from_json_file import load_from_json_file
+if __name__ == "__main__":
+    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('8-load_from_json_file').load_from_json_file
 
-if __name__ == '__main__':
-    file_name = 'add_item.json'
-    my_list = []
-    if path.isfile(file_name):
-        my_list = load_from_json_file(file_name)
-    my_list.extend(sys.argv[1:])
-    save_to_json_file(my_list, file_name)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
