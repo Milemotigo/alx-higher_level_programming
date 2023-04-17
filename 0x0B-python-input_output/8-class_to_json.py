0bcd9006ec33a473de38781e4865c6ba72004af3
milemotigo <mtigo3617@gmail.com>
2023-04-17 12:35:41 -0700

    classtojson

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,20 +1,11 @@
 #!/usr/bin/python3
-'''converts fron object to json'''
-import json
+"""
+Module for class_to_json method.
+"""
 
 
 def class_to_json(obj):
-    '''returns the dictionary description with simple data
-    Arg:
-        valid: returns a list of valid attributes and method
-        value: returns attribute of obj
-    '''
-    Output = {}
-    valid = dir(obj)
-
-    for attr in valid:
-        value = getattr(obj, attr)
-
-        if not callable(value) and not attr.startswith('__'):
-            Output[attr] = value
-    return Output.__dict__
+    """
+        returns dictionary representation with simple data structure.
+    """
+    return obj.__dict__
