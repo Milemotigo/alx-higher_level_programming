7180abcc904ff48deae15e250db066ea90ba751c
milemotigo <mtigo3617@gmail.com>
2023-04-19 15:27:51 -0700

    baseclass

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+'''defines a base class'''
 
 
 class Base():
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+from Base import base
+
+class Rectangle(Base):
+    def '__name__' == '__main__':
+    def __init__(self, width, height, x=0, y=0, id=None):
+
+    @property setter
+
