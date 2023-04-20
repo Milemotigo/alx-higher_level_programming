aa6a4d53671f1805f277580df216b09358d1d2fa
milemotigo <mtigo3617@gmail.com>
2023-04-19 17:01:31 -0700

    rectangles

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,41 @@
 #!/usr/bin/python3
+'''inherit from base clase for rectangle'''
 
 
-from Base import base
+from models.base import Base
 
 class Rectangle(Base):
-    def '__name__' == '__main__':
     def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
-    @property setter
+    @property
+    def width(self):
+        return self.__width
+    @width.setter
+    def width(self, value):
+        self.__width = value
 
+    @property
+    def height(self):
+        return self.__height
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+    @x.setter
+    def x(self, value):
+        __self.x = value
+
+    @property
+    def y(self):
+        return self.__y
+    @y.setter
+    def y(self, value):
+        self.__y = value
