c1a151501d28a8903235b28daf0e5bd3f06cc428
milemotigo <mtigo3617@gmail.com>
2023-04-22 22:04:58 -0700

    commit-12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,5 +12,15 @@ class Square(Rectangle):
 
     def __str__(self):
         '''Returns string info about this square.'''
-        return '[Square] ({}) {}/{} - {}'.\
-            format(self.id, self.x, self.y, self.width)
+        return '[Square] ({}) {}/{} - {}'.format(
+                self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        '''getters or setters of squaer'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
