564ca0067573f77c23c0914bbffc3363b886cb55
milemotigo <mtigo3617@gmail.com>
2023-05-17 08:16:19 -0700

    mysql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,10 @@
 -- Lists all record of the table order by score
-
 SELECT second_table.name, second_table.score
-FROM second_table
-ORDER BY 2 DESC;
+FROM second_table ORDER BY score DESC;
+
+-- a script that lists all records of a table
+-- lists all records of the table second_table of the database hbtn_0c_0
+-- SELECT
+--    score, name
+-- FROM
+--    second_table ORDER BY score DESC;
