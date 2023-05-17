1b2fe08624b94538fd3b96ca41ee92be84f207ae
milemotigo <mtigo3617@gmail.com>
2023-05-17 08:12:21 -0700

    mysql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- Lists all record of the table
 
-SELECT name, score
-FROM second_table AS st
-ORDER BY score DESC;
+SELECT second_table.name, second_table.score
+FROM second_table
+ORDER BY 2 DESC;
