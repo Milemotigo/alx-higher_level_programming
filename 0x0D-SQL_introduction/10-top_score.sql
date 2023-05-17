bdd024671b73ba4080159d298555bd4b2c82810a
milemotigo <mtigo3617@gmail.com>
2023-05-17 08:21:20 -0700

    mysql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -2,9 +2,8 @@
 --SELECT second_table.name, second_table.score
 --FROM second_table ORDER BY score DESC;
 
--- a script that lists all records of a table
--- lists all records of the table second_table of the database hbtn_0c_0
-SELECT
-     score, name
-FROM
-    second_table ORDER BY score DESC;
+-- Lists all records of the table second_table.
+-- Records are ordered by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
