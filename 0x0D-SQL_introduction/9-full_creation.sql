6360de1dff6fd94748a53a0379175234d2cb1bbc
milemotigo <mtigo3617@gmail.com>
2023-05-17 06:11:28 -0700

    show tabke

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -11,7 +11,7 @@ second_table(
 
 INSERT INTO second_table(id, name score) values
 	(1, "John", 10)
-	(2, 'Alex', 3)
-	(3, 'Bob', 14)
-	(4, 'George', 8)
+	(2, "Alex", 3)
+	(3, "Bob", 14)
+	(4, "George", 8)
 );
