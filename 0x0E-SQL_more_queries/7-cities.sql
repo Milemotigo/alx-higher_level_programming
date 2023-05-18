fd456aa295a7ad5e918090d4740cf492824f3e5f
milemotigo <mtigo3617@gmail.com>
2023-05-18 15:05:33 -0700

    reference

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,6 @@
 -- Script that creates hbtn_0d_usa
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 -- Scripit that creates the cities
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT UNIQUE AUTO_INCREMENT
