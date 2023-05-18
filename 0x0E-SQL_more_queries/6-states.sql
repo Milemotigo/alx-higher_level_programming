2d966cca29cd4c7aef635a18f706708ffcffb228
milemotigo <mtigo3617@gmail.com>
2023-05-18 13:38:26 -0700

    Task4

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Script that creates the table id_not_null
 CREATE TABLE IF NOT EXISTS unique_id(
-	id INT NOT NULL UNIQUE PRIMARY KEY,
+	id INT UNIQUE PRIMARY KEY NOT NULL,
 	name VARCHAR(256)  NOT NULL
 	);
