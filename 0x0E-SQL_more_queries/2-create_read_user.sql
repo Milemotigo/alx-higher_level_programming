8930a30058f5ebf7e29d4101c9e90e944b8ea311
milemotigo <mtigo3617@gmail.com>
2023-05-18 12:17:40 -0700

    Task2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Script that creates the MySQL server user user_0d_2,  htbn_0d_2
+-- Script that creates the MySQL server user user_0d_2, htbn_0d_2
 -- Grant mysql user all privileges
 CREATE DATABASE IF NOT EXISTS htbn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
