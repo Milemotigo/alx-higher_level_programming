aec4f31ebfb2ccbc4b8509eef993df61f109367d
milemotigo <mtigo3617@gmail.com>
2023-05-18 13:47:13 -0700

    Task4

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Script that creates the table id_not_null
-CREATE TABLE IF NOT EXISTS states(
+CREATE TABLE IF NOT EXISTS hbtn_0d_usd.states(
 	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
 	name VARCHAR(256)  NOT NULL
 	);
