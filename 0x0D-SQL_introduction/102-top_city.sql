289de1c10cbfd079c89480c22a5110755b219c41
milemotigo <mtigo3617@gmail.com>
2023-05-17 23:27:52 -0700

    gg

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,8 +1,8 @@
 -- compute the average temperature of a city
 
-SELECT `city`, AVG(`value`) AS `avg_temp`
-FROM `temperatures`
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
 WHERE month = 7 AND month = 8
-GROUP BY `city`
+GROUP BY city
 ORDER BY 2 DESC
 LIMIT 3;
