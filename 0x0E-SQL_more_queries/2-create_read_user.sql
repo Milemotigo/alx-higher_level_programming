a01c6fff7fe7bf599981ee44c57dcb4f423740a6
milemotigo <mtigo3617@gmail.com>
2023-05-18 12:21:28 -0700

    selec

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- Script that creates the MySQL server user user_0d_2, htbn_0d_2
 -- Grant mysql user select
-CREATE DATABASE IF NOT EXISTS htbn_0d_2;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON htbn_0d_2.* TO user_0d_2@localhost;
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
