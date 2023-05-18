ae40ed3ade2f469140fcf146c9d165482502fc50
milemotigo <mtigo3617@gmail.com>
2023-05-18 15:10:13 -0700

    reference

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,8 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 -- Scripit that creates the cities
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
-	id INT UNIQUE AUTO_INCREMENT
-	PRIMARY KEY(id) NOT NULL,
+	id INT UNIQUE AUTO_INCREMENT NOT NULL,
+	PRIMARY KEY(id),
 	state_id INT NOT NULL,
 	FOREIGN KEY(state_id),
 	REFERENCES states(state_id) NOT NULL
