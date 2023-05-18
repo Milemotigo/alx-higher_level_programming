0fb49baa12d61a3d33c99ed567081844ff328e91
milemotigo <mtigo3617@gmail.com>
2023-05-18 14:15:33 -0700

    Task4

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,6 +3,6 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 -- Script that creates the cities
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
 	id INT UNIQUE AUTO_INCREMENT PRIMARY KEY NOT NULL,
-	FOREIGN KEY (state_id INT) NOT NULL,
+	FOREIGN KEY(state_id INT) NOT NULL,
 	name VARCHAR(256)  NOT NULL
 	);
