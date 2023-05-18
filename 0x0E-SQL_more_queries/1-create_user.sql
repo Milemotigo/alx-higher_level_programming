d5c19f2e2a094328e98abc5fa2249967429dfd7c
milemotigo <mtigo3617@gmail.com>
2023-05-18 11:21:48 -0700

    grant

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- script that creates the MySQL server user user_0d_1
 -- Grant mysql user all privileges
-IF NOT user_0d_1 EXISTS
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd'
-GRANT user_0d_1*;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH authentication_plugin BY 'user_0d_1_pwd';
+GRANT 'user_0d_1'@'localhost' *;
