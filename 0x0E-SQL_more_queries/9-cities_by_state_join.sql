571594152316ce694fadfbb00ccf4389b2c8ef72
milemotigo <mtigo3617@gmail.com>
2023-05-18 22:38:58 -0700

    cities

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 -- Lists all cities  in the database hbtn_0d_usa.
 -- Results are ordered by ascending cities.id.
-SELECT cities.`id`, cities.`name`
+SELECT `id`, `name`
 FROM `cities`
-JOIN `states` ON cities.`state_id` = states.`id`
-WHERE states.`name` = "California";
+JOIN `states` ON `state_id` = `id`
+WHERE `name` = "California";
