6560cc363974adbc871835b678af279165352fde
milemotigo <mtigo3617@gmail.com>
2023-05-18 22:58:39 -0700

    cities

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- Lists all cities  in the database hbtn_0d_usa.
 -- Results are ordered by ascending cities.id.
-SELECT cities`id`, cities.name, states.`name`
-FROM `cities`
-INNER JOIN `states` ON cities.`state_id` = state.`id`
+SELECT cities.id, cities.name, states.name
+FROM cities
+INNER JOIN states ON cities.state_id = state.id;
