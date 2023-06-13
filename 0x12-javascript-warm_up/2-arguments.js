90f359edc4ab62c421533cc166eb00d246f6ef63
milemotigo <mtigo3617@gmail.com>
2023-06-13 16:15:00 -0700

    argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,10 @@
 
 // arguments passed
 const args = process.argv.length - 2;
-if (args === 0){
-	console.log('No argument');
-}
-else if (args === 1){
-	console.log('Argument found');
-}
-else{
-	console.log('Arguments found');
+if (args === 0) {
+  console.log('No argument');
+} else if (args === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
