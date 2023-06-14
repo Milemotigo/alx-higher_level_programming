73e81b80de57122837dc7797d533e6b549abb5d5
milemotigo <mtigo3617@gmail.com>
2023-06-14 07:29:30 -0700

    task 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,6 +2,10 @@
 
 // prints x times “C is fun”
 const args = process.argv.slice(2);
+const count = parseInt(args);
+if (!count) {
+	console.log("Missing number of occurrences");
+}
 for (let i = 0; i < args; i++) {
   console.log('C is fun');
 }
