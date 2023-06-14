703093e7a8f20d083cad3ac9880c30ad4d4ebed2
milemotigo <mtigo3617@gmail.com>
2023-06-14 09:19:38 -0700

    task9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,12 +5,10 @@ const args = process.argv.slice(2);
 const num1 = parseInt(args[0]);
 const num2 = parseInt(args[1]);
 if (args.length <= 1) {
-    console.log('NaN');
-}
-else if (!num1 || !num2) {
-    console.log('NaN');
-}
-else {
-    let add = num1 + num2;
-    console.log(add);
+  console.log('NaN');
+} else if (isNaN(num1) || isNaN(num2)) {
+  console.log('NaN');
+} else {
+  const add = num1 + num2;
+  console.log(add);
 }
