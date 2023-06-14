6de026a1fcc81834c7c8a3871e1b055def9da623
Milemotigo <mtigo@gmail.com>
2023-06-14 17:10:46 +0100

    task 8

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,11 +2,15 @@
 
 // prints the addition of 2 integers
 const args = process.argv.slice(2);
-const nums = parseInt(args);
-if (!nums || args.length <= 1) {
+const num1 = parseInt(args[0]);
+const num2 = parseInt(args[1]);
+if (args.length <= 1) {
     console.log('NaN');
 }
-else if (args > 1){
-    let add = parseInt(args[0]) + parseInt(args[1]);
+else if (!num1 || !num2) {;
+    console.log('NaN');
+}
+else {
+    let add = num1 + num2;
     console.log(add);
 }
