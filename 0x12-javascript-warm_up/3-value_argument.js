45683f63a5f7c54f3567911595a31f1ea0853636
milemotigo <mtigo3617@gmail.com>
2023-06-13 22:16:43 -0700

    count

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,21 +1,9 @@
 #!/usr/bin/node
 
-// fisx
-function len (str) {
-  let count = 0;
-  let char = str[count];
-  while (char !== undefined) {
-    count++;
-    char = str[count];
-  }
-  return count;
-}
-
-const args = process.argv.slice(2);
-const argCount = len(args);
-
-if (argCount === 0) {
+// args
+let argCount = process.argv;
+if (argCount[2] === null) {
   console.log('No argument');
-} else if (argCount === 1) {
-  console.log(`${args}`);
+} else if (argCount[2]) {
+  console.log(`${argCount[2]}`);
 }
