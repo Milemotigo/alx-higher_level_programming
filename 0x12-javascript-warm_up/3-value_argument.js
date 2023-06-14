dba2811401a6fa29493918afec5204b9fbe44d79
milemotigo <mtigo3617@gmail.com>
2023-06-13 21:57:54 -0700

    wierd

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+function len(str) {
+  let count = 0;
+  let char = str[count];
+  while (char !== undefined) {
+    count++;
+    char = str[count];
+  }
+  return count;
+}
+
+const args = process.argv.slice(2);
+const argCount = len(args);
+
+if (argCount === 0) {
+  console.log('No argument');
+} else if (argCount === 1) {
+  console.log(`${args}`);
+}
