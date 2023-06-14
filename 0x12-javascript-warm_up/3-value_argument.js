f28fab49453a036c3bc0ddfe58cb63a5ba12c926
milemotigo <mtigo3617@gmail.com>
2023-06-13 22:29:19 -0700

    count

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,21 @@
 #!/usr/bin/node
 
 // args
-let argCount = process.argv;
-if (argCount[2] === null) {
+function len (str) {
+  let count = 0;
+  let char = str[count];
+  while (char !== undefined) {
+    count++;
+    char = str[count];
+  }
+  return count;
+}
+
+const args = process.argv.slice(2);
+const argCount = len(args);
+
+if (argCount === 0) {
   console.log('No argument');
-} else if (argCount[2]) {
-  console.log(`${argCount[2]}`);
+} else if (argCount === 1) {
+  console.log(`${args}`);
 }
