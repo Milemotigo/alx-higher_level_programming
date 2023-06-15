023ece79330ffa91e07f63b0c61aa78f3635c499
Milemotigo <mtigo@gmail.com>
2023-06-15 21:51:45 +0100

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
-const Square$ = require('./5-square');
+const SquareX = require('./5-square');
 
-// child class
-class Square extends Square$ {
-  charPrint(c) {
-      if (c === undefined) {
-          c = 'X';
-      }
-          for (let i = 0; i < this.length; i++)
-          {
-              console.log(c.repeat(this.width));
-            }
+class Square extends SquareX {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
     }
+    for (let i = 0; i < this.height; i++) {
+      let s = c.repeat(this.width);
+      console.log(s);
+    }
+  }
 }
 module.exports = Square;
