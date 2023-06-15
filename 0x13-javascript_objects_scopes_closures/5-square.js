baaa153697071abdc414959851b3b10ba6d09835
Milemotigo <mtigo@gmail.com>
2023-06-15 13:42:00 +0100

    task4

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+// child class
+class Square extends Rectangle {
+    constructor(size) {
+        super(size)
+        this.size = size;
+    }
+    square() {
+        this.size *= this.size;
+    }
+}
+module.exports = Square;
