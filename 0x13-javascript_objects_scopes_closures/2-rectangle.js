ba433d1f06c42bfd0667b1fed488a4084b47eed7
Milemotigo <mtigo@gmail.com>
2023-06-15 11:03:19 +0100

    task 3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -3,7 +3,7 @@
 // class Rectangle
 class Rectangle {
   constructor (w, h) {
-    if (h === 0 || w === 0 || h <= 0 || w <= 0) {
+    if (h >= 0 && w >= 0) {
       return;
     }
     this.width = w;
---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+// class Rectangle
+class Rectangle {
+    constructor (w, h) {
+        if (h >= 0 || w >= 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+    print() {
+        let rectange = '';
+        let right = '';
+        for (let row = 0; row < this.height; row++)
+        {
+            for (let col = 0; col < this.width; col++)
+            {
+                rectange += 'X';
+            }
+            rectange += '\n';
+        }
+        console.log(rectange);
+    }
+}
+module.exports = Rectangle;
