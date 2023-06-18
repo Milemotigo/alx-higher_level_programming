611be4c312971e09edb14d887495af55ce33f0ec
milemotigo <mtigo3617@gmail.com>
2023-06-18 13:00:09 -0700

    sql

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,23 @@
+import MySQLdb
+
+db = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user="mysql username",
+    passwd="mysql password",
+    db="hbtn_0e_0_usa"
+)
+
+cursor = db.cursor()
+
+connect = "SELECT * FROM states ORDER BY states.id ASC"
+
+cursor.execute(connect)
+
+results = cursor.fetchall()
+
+for row in results:
+    print(row)
+
+cursor.close()
+db.close()
