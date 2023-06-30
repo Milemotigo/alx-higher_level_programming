63b6cd937a76fe64b3453c548914e497eaa20805
milemotigo <mtgo@gmail.com>
2023-06-30 04:38:30 -0700

    task 0

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # send request to URL, display size of response body in bytes
-curl -sI "$1" | grep content-length | cut -d " " -f 2
+curl -sI "$1" | grep content-length | cut -d ":" -f 2
