d2f69f4c582714b70b183881d7c5498fb3a5cae5
milemotigo <mtigo@gmail.com>
2023-06-30 11:10:43 -0700

    task4

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script that takes in a URL and displays all HTTP methods the server will access
-curl -s -I "$1" | grep -i allow | cut -d' ' -f2-
+#curl -s -I "$1" | grep -i allow | cut -d' ' -f2-
