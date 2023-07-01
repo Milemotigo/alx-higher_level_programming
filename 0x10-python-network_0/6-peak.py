d37f6d8a4b4f0241616aa742b04e52f9713be67a
milemotigo <mtigo3617@gmail.com>
2023-07-01 05:12:34 -0700

    task 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python3
-#  function that finds a peak in a list of unsorted integers
-def find_peak(list_of_integers):
-    """
-    Find a peak in a list of unsorted integers.
+"""Defines a peak-finding algorithm"""
 
-    :param list_of_integers: A list of unsorted integers.
-    :return: The peak value in the list.
-    """
+def find_peak(list_of_integers):
     if list_of_integers:
         peak = list_of_integers[:-1]
         Max_num = peak[-1]
