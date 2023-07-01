9230ea581fd2c69c21169c95892e381237491dcb
milemotigo <mtigo3617@gmail.com>
2023-07-01 12:16:58 -0700

    tas1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 # python script that fetches https://alx-intranet.hbtn.io/status
-if __name__ == '__main__':
+if __name__ == "__main__":
     import urllib.request
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as response:
         body = response.read()
         utf8_content = body.decode('utf8')
         print('Body response:')
-        print('\t - type:', type(body))
-        print('\t - content:', body)
-        print('\t - utf8 content:', utf8_content)
+        print('\t- type:', type(body))
+        print('\t- content:', body)
+        print('\t- utf8 content:', utf8_content)
