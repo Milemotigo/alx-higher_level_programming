5a2bad1b4ecbf87880713fdb8c4880e976f59c5f
milemotigo <mtigo3617@gmail.com>
2023-07-03 06:46:47 -0700

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,17 +2,17 @@
 '''
 Python script that takes in a URL, sends a request to the URL
 '''
-import urllib.request
-import sys
-
-def display_url(url):
-    try:
-        with urllib.request.urlopen(url) as response:
-            result = response.read()
-            print(result.decode("utf-8"))
-    except urllib.error.HTTPError as e:
-        print("Error code: {}".format(e.code))
-
 if __name__ == "__main__":
-    url = sys.argv[1]
-    display_url(url)
+    import urllib.request
+    import sys
+
+    def display_url(url):
+        try:
+            with urllib.request.urlopen(url) as response:
+                result = response.read()
+                print(result.decode("utf-8"))
+        except urllib.error.HTTPError as e:
+            print("Error code: {}".format(e.code))
+
+        url = sys.argv[1]
+        display_url(url)
