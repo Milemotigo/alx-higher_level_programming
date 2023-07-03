26b50b3ea889f677c7e38dda560d7c11268f9008
milemotigo <mtigo3617@gmail.com>
2023-07-03 01:13:00 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 '''
-Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id
+Python script that takes in a URL, sends a request \
+        to the URL and displays the value of the X-Request-Id
 '''
+
 
 import urllib.request
 import sys
+
+
 def take_url(url):
     with urllib.request.urlopen(url) as resp:
         headers = resp.info()
         x_request_id = headers.get("X-Request-Id")
-        if x_request_id:
-            print(f"X-Request-Id found {x_request_id}")
-        else:
-            print("X-Request-Id not found")
+        print(x_request_id)
+
 
 url = sys.argv[1]
 take_url(url)
-
