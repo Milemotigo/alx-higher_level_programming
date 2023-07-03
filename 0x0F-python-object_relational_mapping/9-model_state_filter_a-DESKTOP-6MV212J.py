8a38c3f12ca3ea64c38612478779db2776d1631c
milemotigo <mtigo3617@gmail.com>
2023-07-03 02:46:50 -0700

    task2

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a-DESKTOP-6MV212J.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+
+if __name__ == "__main__":
+    engine = create_engine(f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost/{sys.argv[3]}')
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like("%a%")).order_by(State.id).first()
+
+    if states is None:
+        print('result not found')
+    else:
+        print(f'{states.id}: {states.name}')
