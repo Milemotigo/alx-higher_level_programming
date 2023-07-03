765aab9bce609f0ad41c35dbb84af84f3e2df9a7
milemotigo <mtigo3617@gmail.com>
2023-07-03 01:28:24 -0700

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,18 +2,17 @@
 '''
 Python script that takes in a URL, sends a request tothe url
 '''
+if __name__ == "__main__":
+    import urllib.request
+    import sys
 
 
-import urllib.request
-import sys
+    def take_url(url):
+        with urllib.request.urlopen(url) as resp:
+            headers = resp.info()
+            x_request_id = headers.get("X-Request-Id")
+            print(x_request_id)
 
 
-def take_url(url):
-    with urllib.request.urlopen(url) as resp:
-        headers = resp.info()
-        x_request_id = headers.get("X-Request-Id")
-        print(x_request_id)
-
-
-url = sys.argv[1]
-take_url(url)
+    url = sys.argv[1]
+    take_url(url)
