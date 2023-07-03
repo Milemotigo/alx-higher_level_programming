0b59e1cd82757106566061d46267867c971c4060
milemotigo <mtigo3617@gmail.com>
2023-07-03 05:42:43 -0700

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-""" 
- This is MODULE:2 - This script takes in a URL + email, sends POST 
- request to the passed URL with the email as a parameter, 
- and displays the body of the response (decoded in utf-8) 
- """
-import urllib.parse
-import urllib.request
-import sys
-
-if __name__ == "__main__":
-    def send_email(url, email):
-        email_json = {"email": email}
-        email = urllib.parse.urlencode(email_json)
-
-        data = email.encode("utf-8")
-        seek = url.request.Request(url, data)
-        with urllib.request.urlopen(seek) as response:
-            display_body = response
-            print(display_body.decode("utf-8"))
+'''
+A Python script that takes in a URL and an email, sends a POST
+request to the passed URL with the email as a parameter, and
+displays the body of the response (decoded in utf-8)
+'''
 
 
-    url = sys.argv[1]
-    email = sys.argv[2]
-    send_email(url, email)
+if _name_ == "_main_":
+    import sys
+    import urllib.parse
+    import urllib.request
+
+    url, email = sys.argv[1:3]
+    value = {"email": email}
+
+    data = urllib.parse.urlencode(value).encode("ascii")
+
+    req = urllib.request.Request(url, data)
+
+    with urllib.request.urlopen(req) as resp:
+        print(resp.read().decode("utf-8"))
