3c5a4b71fdeb009c1bb2235e94386638ee20d6a9
milemotigo <mtigo@gmail.com>
2023-07-05 15:38:24 +0100

    task2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,17 +4,17 @@
  request to the passed URL with the email as a parameter, 
  and displays the body of the response (decoded in utf-8) 
  """
-import urllib.parse
-import urllib.request
-import sys
-
 if __name__ == "__main__":
+    import urllib.parse
+    import urllib.request
+    import sys
+
     def send_email(url, email):
         email_json = {"email": email}
         email = urllib.parse.urlencode(email_json)
 
         data = email.encode("utf-8")
-        seek = url.request.Request(url, data)
+        seek = url.request.Request(url, data = data, method = 'POST')
         with urllib.request.urlopen(seek) as response:
             display_body = response
             print(display_body.decode("utf-8"))
