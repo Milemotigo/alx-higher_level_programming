496db6c840893fee7d5ddbb733d612df9270bb8f
milemotigo <mtgo@gmail.com>
2023-07-05 10:49:52 -0700

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 '''
 Python script that takes in a URL, sends a request to the URL
+and also handles errors
 '''
 if __name__ == "__main__":
     import urllib.request
+    import urllib.error
     import sys
 
     def display_url(url):
