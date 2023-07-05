90b4c824235e290521a3722e39f175915471825c
milemotigo <mtigo@gmail.com>
2023-07-05 14:11:17 +0100

    task 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,13 +6,10 @@ if __name__ == "__main__":
     import urllib.request
     import sys
 
-
     def take_url(url):
         with urllib.request.urlopen(url) as resp:
             headers = resp.info()
             x_request_id = headers.get("X-Request-Id")
             print(x_request_id)
-
-
     url = sys.argv[1]
     take_url(url)
