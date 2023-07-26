90d73b0a0151425c22c17f0b48f21af84ff9e15d
milemotigo <mtigo@gmail.com>
2023-07-26 19:36:46 +0100

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv[2]
+const url = `https://swapi-api.hbtn.io/api/films/${args[0]}`;
+
+request(url, function(err, response, body) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
