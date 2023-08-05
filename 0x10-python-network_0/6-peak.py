0516caa9c05e6adf0ea404a0018ce22373834389
milemotigo <mtigo3617@gmail.com>
2023-08-04 19:45:06 -0700

    task 10

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,13 +2,13 @@
 """Defines a peak-finding algorithm"""
 
 
-#def find_peak(list_of_integers):
-#    if list_of_integers:
-#       peak = list_of_integers[:-1]
-#        Max_num = peak[-1]
-#        return(Max_num)
-#    else:
-#        return None
+def find_peak(list_of_integers):
+   if list_of_integers:
+       peak = list_of_integers[:-1]
+       Max_num = peak[-1]
+       return(Max_num)
+   else:
+      return None
 
 def find_peak(list_of_integers):
     """
@@ -22,10 +22,4 @@ def find_peak(list_of_integers):
     elif len(list_of_integers) == 1:
         return list_of_integers[0]
     else:
-        return None
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))    
+        return None   
---
+++ 0x14-javascript-web_scraping/test.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+arg0 = process.argv[0]
+arg1 = process.argv[1]
+arg2 = process.argv[2]
+arg3 = process.argv[3]
+
+console.log(arg0)
+console.log(arg1)
+console.log(arg2)
+console.log(arg3)
